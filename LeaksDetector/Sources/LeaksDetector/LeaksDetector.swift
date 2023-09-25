dac1b1498cf39a94741c765cf9e7ff46ccad26c4
Hoang Anh Tuan <tuanhanh19981998@gmail.com>
2023-09-25 23:11:15 +0800

    Update Leaks Detector

--- LeaksDetector/Sources/LeaksDetector/LeaksDetector.swift
+++ LeaksDetector/Sources/LeaksDetector/LeaksDetector.swift
@@ -1,4 +1,5 @@
 
+import Foundation
 import ArgumentParser
 import ShellOut
 
@@ -8,63 +9,65 @@ import ShellOut
 @main
 struct LeaksDetector: ParsableCommand {
     
-//    @Argument(help: "The name of the running process")
-//    private var processName: String
-
-//    @Option(name: .shortAndLong, help: "The week of the blog post as used in the file name")
-//    private var week: Int?
+    @Argument(help: "The name of the running process")
+    private var processName: String
 
     @Flag(name: .long, help: "Show extra logging for debugging purposes")
     private var verbose: Bool = false
     
-    private var memgraphFileName = "LeaksDemo.memgraph"
-    
-    private var regexes: [String] = [
-        ".*(\\d+) leaks for (\\d+) total leaked bytes.*",
-        ".*MemoryLeaksCheck.*",
-        ".*ROOT LEAK.*"
-    ]
+    private var memgraphFileName = "Leaks.memgraph"
+    private var regex: String = ".*(\\d+) leaks for (\\d+) total leaked bytes.*"
     
     func run() throws {
         do {
-//            try shellOut(to: "leaks MemoryLeaksCheck --outputGraph=~/Desktop/LeaksDemo.memgraph")
-//            print("Finish create memgraph")
-            try shellOut(to: "leaks", arguments: ["~/Desktop/LeaksDemo.memgraph -q"])
+            debugPrint("Process name: \(processName)")
+            
+            try shellOut(to: "leaks \(processName) --outputGraph=~/Desktop/\(memgraphFileName)")
+            debugPrint("Generate memgraph successfully for process 🚀")
+            
+            try shellOut(to: "leaks", arguments: ["~/Desktop/\(memgraphFileName) -q"])
         } catch {
             let error = error as! ShellOutError
             if error.output.isEmpty { return }
 
             let inputs = error.output.components(separatedBy: "\n")
-            var res: [String] = []
-
-            inputs.forEach {
-                for regex in regexes {
-                    if $0.matches(regex) {
-                        res.append($0)
-                        break
-                    }
-                }
-            }
+            guard let numberOfLeaksMessage = inputs.first(where: { $0.matches(regex) }) else { return }
+            let numberOfLeaks = getNumberOfLeaks(from: numberOfLeaksMessage)
+            
+            if numberOfLeaks < 1 { return }
             
             // Create a file to store the message, so that later Danger can read from that file
             // TODO: Convert to 1 message instead of using array-for loop to optimize execution time
-            if !res.isEmpty {
-                let fileName = "temporary.txt"
-                for message in res {
-                    let updatedMessage = "\"\(message)\""
-                    try shellOut(to: "echo \(updatedMessage) >> \(fileName)")
-                }
+            
+            let fileName = "temporary.txt"
+            for message in inputs {
+                let updatedMessage = "\"\(message)\""
+                try shellOut(to: "echo \(updatedMessage) >> \(fileName)")
             }
             
             // Cache memgraphfile if need
             
             // Execute Danger
             /// Split to multiple Danger instance: https://www.jessesquires.com/blog/2020/12/15/running-multiple-dangers/
-            try shellOut(to: "bundle exec danger --dangerfile=Dangerfile.leaksReport --danger_id=LeaksReport")
+//            try shellOut(to: "bundle exec danger --dangerfile=Dangerfile.leaksReport --danger_id=LeaksReport")
             
             // Clean up
             // TODO: Remove memgraph & temporary.txt
         }
+    }
+    
+    private func getNumberOfLeaks(from message: String) -> Int {
+        if let regex = try? NSRegularExpression(pattern: regex, options: []) {
+            // Find the first match in the input string
+            if let match = regex.firstMatch(in: message, options: [], range: NSRange(message.startIndex..., in: message)) {
+                // Extract the "d" value from the first capture group
+                if let dRange = Range(match.range(at: 1), in: message), let dValue = Int(message[dRange]) {
+                    return dValue
+                }
+            }
+        }
+        
+        return 0
     }
     
 }
